1e034b30e570f1aed82861358cbcfe162ce3c64d
mesfint <mesfin_meng@yahoo.com>
2023-10-16 09:13:57 +0300

    Displaying Critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+  let color = score > 75 ? "green" : score > 60 ? "Yellow" : "";
+  return (
+    <div>
+      <Badge
+        fontSize={"14px"}
+        paddingX={2}
+        borderRadius={"4px"}
+        colorScheme={color}
+      >
+        {score}
+      </Badge>
+    </div>
+  );
+}
+
+export default CriticScore;
