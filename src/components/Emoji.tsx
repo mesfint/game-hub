c37e5436efd169e6a09867ca44e1dfe2d7d16c8c
mesfint <mesfin_meng@yahoo.com>
2023-10-18 13:10:24 +0300

    Add Emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,26 @@
+import bullsEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+
+function Emoji({ rating }: Props) {
+  if (rating < 3) return null;
+
+  //Map obj
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "25px" },
+  };
+  //   since we are spreading all object of emojiMap
+  // every properties are impeded inside Image automatically
+  //meaning all the images will have same 25px
+
+  return <Image {...emojiMap[rating]} marginY={1} />;
+}
+
+export default Emoji;
