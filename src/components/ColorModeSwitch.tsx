03e6e92ce0ea8794ab6a431dca3ee328f4f90cc7
mesfint <mesfin_meng@yahoo.com>
2023-10-18 10:45:19 +0300

    Fix the Issue with chakra Menu

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,11 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  HStack,
+  Switch,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 
 function ColorModeSwitch() {
   // The hook useColorMode let you update color mode of your app in every cases.
@@ -7,14 +14,15 @@ function ColorModeSwitch() {
 
   return (
     // horizontal stack
-    <HStack>
+    <Flex paddingLeft={2} marginRight={5}>
       <Switch
+        marginRight={5}
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       ></Switch>
       <Text>Dark Mode</Text>
-    </HStack>
+    </Flex>
   );
 }
 
