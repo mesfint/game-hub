568309f0358fd7ab670092950754613e94f70687
mesfint <mesfin_meng@yahoo.com>
2023-10-18 10:28:37 +0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   //Example to crop an image 600 by 400
   //https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
   //This API supports cropping Images on the fly like above, give attn for /crop/600/400/, which is
